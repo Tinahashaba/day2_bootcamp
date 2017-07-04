0207b04d6d161ac0257c9d1e08e19e75402f9107
Tinah Ashaba <tinahashaba@gmail.com>
2017-07-04 13:14:32 +0100

    created base class from which other laptop classes will inherit

---
+++ laptops.py
@@ -0,0 +1,20 @@
+import abc
+
+
+class Laptop(object):
+    __metaclass__ = abc.ABCMeta
+    initial_sale_price = 0
+    battery_life = 0
+
+    def __init__(self, year, HDD, RAM, sold_on):
+        self.year = year
+        self.HDD = HDD
+        self.RAM = RAM
+        self.sold_on = sold_on
+
+    def sale_price(self):
+        """checks if the laptop has been sold and calcuates the price if hasnt been sold"""
+        if self.sold_on is not None:
+            return 'Already sold'
+        else:
+            return 30 * self.battery_life
